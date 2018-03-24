21e950d5d67f7f26aa5266100771e8e37c943778
Titus Fortner <titusfortner@gmail.com>
2018-03-24 01:46:14 -0500

    basic browser test with Appium

--- src/test/AppiumScript.java
+++ src/test/AppiumScript.java
@@ -2,6 +2,7 @@ package test;
 
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
@@ -10,7 +11,7 @@ import java.net.URL;
 public class AppiumScript {
 
     @Test
-    public void openBrowser() throws MalformedURLException {
+    public void signIn() throws MalformedURLException, InterruptedException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("platformName", "Android");
         capabilities.setCapability("platformVersion", "8.1");
@@ -19,6 +20,14 @@ public class AppiumScript {
 
         AndroidDriver driver = new AndroidDriver<>(
                 new URL("http://localhost:4723/wd/hub"), capabilities);
+
+        driver.get("http://a.testaddressbook.com");
+        driver.findElement(By.tagName("button")).click();
+        driver.findElement(By.id("sign-in")).click();
+
+        driver.findElement(By.id("session_email")).sendKeys("user@example.com");
+        driver.findElement(By.id("session_password")).sendKeys("password");
+        driver.findElement(By.name("commit")).click();
 
         driver.quit();
     }
