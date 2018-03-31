845ed1ff17b7b7387f8db094994bc43322093360
Titus Fortner <titusfortner@gmail.com>
2018-03-31 18:25:13 -0500

    use Faker to provide random data

--- src/test/data/UserData.java
+++ src/test/data/UserData.java
@@ -1,11 +1,20 @@
 package test.data;
 
 import org.testng.annotations.DataProvider;
+import com.github.javafaker.Faker;
 
 public class UserData {
     @DataProvider(name = "validUser")
     public static Object[][] valid() {
         return new Object[][] {{ "user@example.com", "password" }};
+    }
+
+    @DataProvider(name = "randomUser")
+    public static Object[][] random() {
+        Faker faker = new Faker();
+        return new Object[][] {
+                { faker.internet().emailAddress(), faker.internet().password() }
+        };
     }
 }
 
