6820442afdf5005034e6f99ac6f7a8d57303e150
Titus Fortner <titusfortner@gmail.com>
2018-04-01 14:33:43 -0500

    Split these out for more parallelization

--- src/test/tests/SignInErrors.java
+++ src/test/tests/SignInErrors.java
@@ -8,16 +8,7 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
-public class SignIn extends BaseTest {
-    @Test
-    public void signInSuccessful() {
-        SignInPage signInPage = SignInPage.visit(driver);
-        signInPage.signIn(UserData.validUser());
-
-        HomePage homePage = new HomePage(driver);
-        assertTrue(homePage.isSignedIn());
-    }
-
+public class SignInErrors extends BaseTest {
     @Test
     public void signInBlankPasswordUnsuccessful() {
         SignInPage signInPage = SignInPage.visit(driver);
---
+++ src/test/tests/SignInSuccess.java
@@ -0,0 +1,19 @@
+package test.tests;
+
+import org.testng.annotations.Test;
+import test.data.UserData;
+import test.pages.HomePage;
+import test.pages.SignInPage;
+
+import static org.testng.Assert.assertTrue;
+
+public class SignInSuccess extends BaseTest {
+    @Test
+    public void signInSuccessful() {
+        SignInPage signInPage = SignInPage.visit(driver);
+        signInPage.signIn(UserData.validUser());
+
+        HomePage homePage = new HomePage(driver);
+        assertTrue(homePage.isSignedIn());
+    }
+}
