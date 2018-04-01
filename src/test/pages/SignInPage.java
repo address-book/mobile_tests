d234f85e6fd011f048cd7ecd974d0afa2c6fce55
Titus Fortner <titusfortner@gmail.com>
2018-03-31 21:38:26 -0500

    Set Up Exercise to Implement Page Objects

---
+++ src/test/pages/SignInPage.java
@@ -0,0 +1,30 @@
+package test.pages;
+
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import test.data.UserData;
+
+public class SignInPage {
+    private AndroidDriver driver;
+
+    public static SignInPage visit(AndroidDriver driver) {
+        // Implement Method
+    }
+
+    private SignInPage(AndroidDriver driver) {
+        // Implement Method
+    }
+
+    public void navigateToSignUp() {
+        // Implement Method
+    }
+
+    public void signIn(UserData data) {
+        // Implement Method
+    }
+}
+
---
+++ src/test/pages/SignUpPage.java
@@ -0,0 +1,26 @@
+package test.pages;
+
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import test.data.UserData;
+
+public class SignUpPage {
+    private AndroidDriver driver;
+
+    public static SignUpPage visit(AndroidDriver driver) {
+        // Implement Method
+    }
+
+    private SignUpPage(AndroidDriver driver) {
+        // Implement Method
+    }
+
+    public void signIn(UserData data) {
+        // Implement Method
+    }
+}
+
