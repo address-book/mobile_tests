aa48f1960b80fcc0fbd6d0bad199dd46639fbe8b
Titus Fortner <titusfortner@gmail.com>
2018-04-03 13:36:44 -0500

    set up for exercise to test for blank password

---
+++ src/test/java/examples/tests/SignInErrorsTests.java
@@ -0,0 +1,40 @@
+package examples.tests;
+
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.testng.Assert.assertEquals;
+
+public class SignInErrorsTests {
+
+    @Test
+    public void signInBlankPasswordUnsuccessful() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("platformVersion", "8.1");
+        capabilities.setCapability("deviceName", "Android_Emulator");
+        capabilities.setCapability("browserName", "Chrome");
+
+        AndroidDriver driver = new AndroidDriver<>(
+                new URL("http://localhost:4723/wd/hub"), capabilities);
+
+        driver.get("http://a.testaddressbook.com");
+        driver.findElement(By.tagName("button")).click();
+        driver.findElement(By.id("sign-in")).click();
+
+        // Add Action Code
+
+        // Add Assertion Code
+
+        driver.quit();
+    }
+
+}
