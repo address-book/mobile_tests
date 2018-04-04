46a3d8fed0fc153e7bd491bd06053b521a90b581
Titus Fortner <titusfortner@gmail.com>
2018-04-04 21:47:42 +0100

    implement Sauce Configs

--- src/test/java/examples/tests/BaseTest.java
+++ src/test/java/examples/tests/BaseTest.java
@@ -8,6 +8,7 @@ import org.yaml.snakeyaml.Yaml;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Map;
@@ -15,6 +16,8 @@ import java.util.Map;
 
 public class BaseTest {
     AndroidDriver driver;
+
+    private Boolean useSauce = System.getProperty("USE_SAUCE") != null;
 
     private static DesiredCapabilities createCapabilities(String value) throws FileNotFoundException {
         FileReader file = new FileReader("src/test/java/examples/config/platforms.yml");
@@ -28,13 +31,32 @@ public class BaseTest {
     }
 
     @BeforeMethod
-    public void setup() throws MalformedURLException, FileNotFoundException {
+    public void setup(Method method) throws MalformedURLException, FileNotFoundException {
+        String url;
+        DesiredCapabilities capabilities;
         String platformProperty = System.getProperty("PLATFORM");
-        String platform = (platformProperty != null) ? platformProperty : "androidChromeLocal";
-        DesiredCapabilities capabilities = createCapabilities(platform);
 
-        driver = new AndroidDriver<>(
-                new URL("http://localhost:4723/wd/hub"), capabilities);
+        if (useSauce) {
+            String platform = (platformProperty != null) ? platformProperty : "androidChromeSauce";
+            capabilities = createCapabilities(platform);
+
+            String USER = System.getenv("SAUCE_USERNAME");
+            String KEY = System.getenv("SAUCE_ACCESS_KEY");
+
+            capabilities.setCapability("name", method.getName());
+            String buildEnv = System.getenv("BUILD_TAG");
+            if (buildEnv != null) {
+                capabilities.setCapability("build", buildEnv);
+            }
+
+            url = "https://" + USER + ":" + KEY + "@ondemand.saucelabs.com/wd/hub";
+        } else {
+            String platform = (platformProperty != null) ? platformProperty : "androidChrome";
+            capabilities = createCapabilities(platform);
+            url = "http://localhost:4723/wd/hub";
+        }
+
+        driver = new AndroidDriver<>(new URL(url), capabilities);
     }
 
     @AfterMethod
