582d15707c87cb5c31be2c9e33064b2d87640d23
Titus Fortner <titusfortner@gmail.com>
2018-04-04 05:18:31 -0400

    Implement Configuration data with serialization and property override

--- src/test/java/examples/tests/SignInErrorsTests.java
+++ src/test/java/examples/tests/SignInErrorsTests.java
@@ -8,22 +8,37 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.Test;
 
+import org.yaml.snakeyaml.Yaml;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Map;
 
-import static org.testng.Assert.assertEquals;
 
 public class SignInErrorsTests {
 
-    @Test
-    public void signInBlankPasswordUnsuccessful() throws MalformedURLException {
+    private static DesiredCapabilities createCapabilities(String value) throws FileNotFoundException {
+        FileReader file = new FileReader("src/test/java/examples/config/platforms.yml");
+        Map<String, Object> platforms = new Yaml().load(file);
+        Map<String, Object> platform = (Map<String, Object>) platforms.get(value);
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("platformVersion", "8.1");
-        capabilities.setCapability("deviceName", "Android_Emulator");
-        capabilities.setCapability("browserName", "Chrome");
+        for (String key: platform.keySet()) {
+            capabilities.setCapability(key, platform.get(key));
+        }
+        return capabilities;
+    }
+
+    @Test
+    public void signInBlankPasswordUnsuccessful() throws MalformedURLException, FileNotFoundException {
+        String platformProperty = System.getProperty("PLATFORM");
+        String platform = (platformProperty != null) ? platformProperty : "androidChromeLocal";
+        DesiredCapabilities capabilities = createCapabilities(platform);
 
         AndroidDriver driver = new AndroidDriver<>(
                 new URL("http://localhost:4723/wd/hub"), capabilities);
--- src/test/java/examples/tests/SignUpTests.java
+++ src/test/java/examples/tests/SignUpTests.java
@@ -5,22 +5,36 @@ import examples.data.*;
 import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
+import org.yaml.snakeyaml.Yaml;
+
 import org.testng.annotations.Test;
-
-import java.net.MalformedURLException;
-import java.net.URL;
-
 import static org.testng.Assert.assertEquals;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
+
+
 public class SignUpTests {
+    private static DesiredCapabilities createCapabilities(String value) throws FileNotFoundException {
+        FileReader file = new FileReader("src/test/java/examples/config/platforms.yml");
+        Map<String, Object> platforms = new Yaml().load(file);
+        Map<String, Object> platform = (Map<String, Object>) platforms.get(value);
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        for (String key: platform.keySet()) {
+            capabilities.setCapability(key, platform.get(key));
+        }
+        return capabilities;
+    }
 
     @Test()
-    public void signInSuccessful() throws MalformedURLException {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("platformVersion", "8.1");
-        capabilities.setCapability("deviceName", "Android_Emulator");
-        capabilities.setCapability("browserName", "Chrome");
+    public void signInSuccessful() throws MalformedURLException, FileNotFoundException {
+        String platformProperty = System.getProperty("PLATFORM");
+        String platform = (platformProperty != null) ? platformProperty : "androidChromeLocal";
+        DesiredCapabilities capabilities = createCapabilities(platform);
 
         AndroidDriver driver = new AndroidDriver<>(
                 new URL("http://localhost:4723/wd/hub"), capabilities);
